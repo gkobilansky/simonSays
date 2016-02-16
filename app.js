a09be2cdad1b04c4b57edbb914c5160021b11fa6
gkobilansky <genek149@gmail.com>
2016-02-16 11:12:48 -0500

    cleaned up app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const SimonSays = {};
     SimonSays.Game = function (element) {
         this.setElement(element);
         this.start = this.start.bind(this);
-
         this.board = new SimonSays.Board();
         this.scoreBoard = new SimonSays.Scoreboard();
         this.background = new SimonSays.Background();
@@ -47,7 +46,7 @@ const SimonSays = {};
         draw: function () {
             this.background.draw(this.element);
             this.board.draw(this.document);
-            this.scoreBoard.draw(this.round, this.board);
+            this.scoreBoard.draw(this.round);
         },
 
         setElement: function (element) {
@@ -59,7 +58,6 @@ const SimonSays = {};
         start: function () {
             this.round = 0;
             this.simonPlays = [];
-            console.log(this.simonPlays);
             this.simonPlay();
         },
 
@@ -69,7 +67,6 @@ const SimonSays = {};
             this.stopListen();
             this.humanPlays.push(move);
             this.board.play(this.humanPlays.slice(-1));
-            console.log(this.humanPlays, this.simonPlays);
             this.comparePlays(move);
         },
 
@@ -83,6 +80,8 @@ const SimonSays = {};
             this.simonPlays.push(Math.floor(Math.random() * 4));
             this.board.play(this.simonPlays);
             this.scoreBoard.update(this.round);
+
+            //wait for buttons to finish playing b4 listening for user clicks
             setTimeout(this.listen, 500 * this.simonPlays.length);
 
         },
@@ -90,38 +89,37 @@ const SimonSays = {};
         comparePlays: function (move) {
 
             //  Should I be calling game.board and game.simonPlay here?
+            // TODO: Refactor
 
             const gameCheck = move === this.simonPlays[this.humanPlays.length - 1];
 
             this.stopListen();
 
-            if (!gameCheck) {
-                console.log('compare', gameCheck);
+            if (!gameCheck) { // You screwed up
                 this.humanPlays = [];
                 this.scoreBoard.woops();
 
-                if (this.strict) {
+                if (this.strict) { // Restart the game, it was set to strict mode
                     setTimeout(game.start, 1000);
                 } else {
                     setTimeout(function () {
                         game.board.play(game.simonPlays);
                         game.scoreBoard.update(game.round);
-                        setTimeout(game.listen, 500 * game.simonPlays.length);
+                        setTimeout(game.listen, 500 * game.simonPlays.length); // wait for playback to finish before listening for clicks
                     }, 1000);
                 }
-            } else if (this.humanPlays.length === this.simonPlays.length) {
+            } else if (this.humanPlays.length === this.simonPlays.length) { // You did it right
 
-                if (this.round >= 20) {
+                if (this.round >= 20) { // 20 round win condition
                     return this.scoreBoard.win();
                 } else {
 
-                    console.log('compare', gameCheck);
-                    setTimeout(function () {
+                    setTimeout(function () { // Take a breather and then Simon goes again
                         game.simonPlay();
                     }, 1000);
                 }
-            } else {
-                this.listen(); // eventlistenr?
+            } else { // Listen after each human click, unless it's simons turn
+                this.listen();
             }
         }
 
@@ -133,7 +131,7 @@ const SimonSays = {};
         this.buttonCount = 4;
         this.boardHtml = document.createElement('div');
         this.buttons = [];
-        this.colors = ['red', 'green', '#cca707', 'blue'];
+        this.colors = ['red', 'green', '#cca707', 'blue']; // default yellow looked too much like the board
         this.sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
                    'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
                    'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
@@ -144,34 +142,29 @@ const SimonSays = {};
     SimonSays.Board.prototype = {
 
         draw: function (element) {
-
             element.body.appendChild(this.boardHtml);
             this.boardHtml.className = 'board';
 
             let i;
             for (i = 0; i < this.buttonCount; i++) {
-
                 this.buttons[i] = new SimonSays.Button();
                 this.buttons[i].color = this.colors[i];
                 this.buttons[i].sound = this.sounds[i];
                 this.buttons[i].num = i;
                 this.buttons[i].draw(this.boardHtml);
             }
-
-            console.log(this);
         },
 
         play: function (moves) {
+            game.stopListen(); // no clicks while playing the moves
 
-            game.stopListen();
 
+            // When Simon plays full array of his plays is passed, when human plays, just the last play is passed. Each move is mapped to the button element
             const nodes = $.map(moves, function (id) {
                 return document.getElementById(id);
             });
 
-            console.log(nodes);
-
-            $(nodes).each(function (i, el) {
+            $(nodes).each(function (i, el) { //fade button in and out and play sound
                 setTimeout(function () {
                     $(el)
                         .animate({
@@ -183,8 +176,6 @@ const SimonSays = {};
                 }, 500 * i);
 
             });
-
-            //            setTimeout(game.listen, 500 * nodes.length);
 
             function playSound() {
                 const val = $(this).val();
@@ -204,7 +195,6 @@ const SimonSays = {};
         this.color = null;
         this.sound = null;
         this.num = null;
-        // this.side = '100px';     -- moved to css
         this.buttonHtml = document.createElement('div');
     };
 
@@ -213,21 +203,18 @@ const SimonSays = {};
             this.buttonHtml.className = 'square ' + this.color;
             $(this.buttonHtml).prop("id", this.num);
             this.buttonHtml.style.backgroundColor = this.color;
-            //    this.squareHtml.style.width = this.side;   -- moved to css
-            //    this.squareHtml.style.height = this.side;    
             board.appendChild(this.buttonHtml);
         }
     };
 
     // Scoreboard
 
-    SimonSays.Scoreboard = function () {
-        this.round = null;
-    };
+    SimonSays.Scoreboard = function () {};
 
-    SimonSays.Scoreboard.prototype = { // creates an element in board div that tracks rounds NOTE: update not working yet
-        draw: function (round, board) {
-            //            this.round = round;
+    SimonSays.Scoreboard.prototype = {
+
+        // creates an element in board div that tracks rounds
+        draw: function (round) {
             this.scoreboardHtml = document.createElement('div');
             this.scoreboardHtml.className = 'scoreboard';
             $(".scoreboardContainer").append(this.scoreboardHtml);
@@ -240,12 +227,10 @@ const SimonSays = {};
 
         woops: function () {
             $(this.scoreboardHtml).html("<h2>WOOPS!</h2>");
-
         },
 
         win: function () {
             $(this.scoreboardHtml).html("<h2>YOU WIN!</h2><p>Click restart to play again</p>");
-
         },
 
     };
